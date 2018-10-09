77998ecc900a12266a1fba48dd7781d434f826b3
Jose Marino <jrmarino@telus.net>
2018-10-09 10:17:40 -0700

    autogrow options fix - still jumpy

--- jquery.textarea.autogrow.js
+++ jquery.textarea.autogrow.js
@@ -6,14 +6,12 @@
 (function ($) {
 	"use strict";
 	
-    $.fn.autogrow = function (options) {
-		
-		var settings = $.extend({ 			
+    $.fn.autogrow = function (options) {					 
+	 return this.each(function () {
+		 var settings = $.extend({ 			
             maxHeight : 0 // no maximum
         }, options );
-	 	 	
-	 return this.each(function () {
-		 
+		
 		// get the DOM element		
 		var el = $(this).get(0);				
 		el.addEventListener('keydown', function(){			
