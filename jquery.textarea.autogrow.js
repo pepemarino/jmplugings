a9f815606532e8f706c9e49d8f1f7f877acc1853
Jose Marino <jrmarino@telus.net>
2018-10-08 23:17:27 -0700

    Remove minHeight on autogrow plugin

--- jquery.textarea.autogrow.js
+++ jquery.textarea.autogrow.js
@@ -8,32 +8,18 @@
 	
     $.fn.autogrow = function (options) {
 		
-		var settings = $.extend({ 
-			minHeight : 0,
+		var settings = $.extend({ 			
             maxHeight : 0 // no maximum
         }, options );
 	 	 	
 	 return this.each(function () {
 		 
 		// get the DOM element		
-		var el = $(this).get(0);
-		
-		if(settings.minHeight > 0){
-			console.log("style.minHeight " + settings.minHeight );
-			el.style.cssText = 'min-height:' + settings.minHeight + 'px;'
-		}
-				
-		el.addEventListener('keydown', function(){
-			
-			if($(this).is("input")) return;
-										
-			setTimeout(function(){
-				if(settings.minHeight > 0){
-					console.log("style.minHeight " + settings.minHeight );
-					el.style.cssText = 'min-height:' + settings.minHeight + 'px;'
-				}
-				
-				if(settings.maxHeight === 0 ||  (el.scrollHeight + 14) < (settings.maxHeight - settings.minHeight)){
+		var el = $(this).get(0);				
+		el.addEventListener('keydown', function(){			
+			if($(this).is("input")) return;									
+			setTimeout(function(){							
+				if(settings.maxHeight === 0 ||  (el.scrollHeight + 14) < settings.maxHeight){
 					el.style.cssText = 'height:auto; padding:0';				
 					el.style.cssText = 'height:' + (el.scrollHeight + 14) + 'px';
 				}
